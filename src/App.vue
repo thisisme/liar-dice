ff16cb868e8d7c0312d514942f6a0e301e58ee29
Mats Olsberg <mats.olsberg@gmail.com>
2019-06-24 21:12:14 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheDice v-for="i in dices.length" :key="i" :dice="dices[i]" @throwdice="newRandomDice(i)"></TheDice>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheDice from './components/TheDice.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TheDice
+  },
+  data () {
+    return {
+      dices: Array.from({ length: 5 }, () => this.getRandomDice())
+    }
+  },
+  methods: {
+    getRandomDice () {
+      const min = Math.ceil(9)
+      const max = Math.floor(14)
+      return Math.floor(Math.random() * (max - min)) + min
+    },
+    newRandomDice (i) {
+      console.log('test')
+    }
   }
 }
 </script>
@@ -24,5 +38,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/TheDice.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <div class="dice" :class="`dice-${dice}`" @click="click()">
+      <img :src="require(`@/assets/dices/${getDiceFace(dice)}.svg`)" alt="">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Dice',
+  props: {
+    dice: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    getDiceFace (dice) {
+      switch (dice) {
+        case 9: {
+          return 'nine'
+        }
+        case 10: {
+          return 'ten'
+        }
+        case 11: {
+          return 'jack'
+        }
+        case 12: {
+          return 'queen'
+        }
+        case 13: {
+          return 'king'
+        }
+        case 14: {
+          return 'ace'
+        }
+      }
+    },
+    click (e) {
+      this.$emit('throwdice')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .dice {
+    width: 10rem;
+    img {
+      width: 100%;
+    }
+  }
+</style>
