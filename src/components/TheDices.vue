3579e5cacebb49149a5bb235b1c188b146c3d003
Mats Olsberg <mats.olsberg@gmail.com>
2019-07-22 10:34:30 +0200

    Finding type of card

--- src/components/TheDices.vue
+++ src/components/TheDices.vue
@@ -20,24 +20,36 @@ export default {
   },
   data () {
     return {
-      dices: Array.from({ length: 5 }, () => this.getRandomDice())
+      dices: Array.from({ length: 5 }, () => this.getRandomDice()),
+      parentDices: [],
+      clickedDices: []
     }
   },
   methods: {
     getRandomDice () {
       const min = Math.ceil(1)
-      const max = Math.floor(6)
+      const max = Math.floor(7)
       return Math.floor(Math.random() * (max - min)) + min
     },
     newRandomDice (i) {
+      if (this.clickedDices.indexOf(i) > -1) {
+        console.log('Already tossed')
+        return
+      }
+      this.clickedDices.push(i)
+      this.parentDices = this.dices
       this.$set(this.dices, i, this.getRandomDice())
-      checkIfBetter(this.dices)
     }
   },
   watch: {
     currentPlayer (newPlayer, oldPlayer) {
       console.log(this.$store.getters['game/player'](newPlayer).name)
       // console.log(`going from ${this.$store.getters['game/player'](oldPlayer)} to ${this.$store.getters['game/player'](newPlayer)}`)
+    },
+    dices (newDices) {
+      console.log(this.parentDices)
+      console.log(newDices)
+      checkIfBetter(this.dices)
     }
   }
 }
--- src/core/game.js
+++ src/core/game.js
@@ -4,7 +4,8 @@ export function checkIfBetter (hand) {
 
 function checkType (hand) {
   // hand = sortDices(hand)
-  console.log(findDuplicates(hand))
+  hand = findDuplicates(hand)
+  console.log(findTypeOfHand(hand))
 }
 
 function sortNumbers (arr) {
@@ -17,9 +18,49 @@ function findDuplicates (hand) {
       [b]: (a[b] || 0) + 1
     }), {})
 
-  const duplicates = dices =>
-    Object.keys(dices).filter((a) => dices[a] > 1)
+  // const duplicates = dices =>
+  //   Object.keys(dices).filter((a) => dices[a] > 1)
 
-  console.log(count(hand))
-  console.log(duplicates(count(hand)))
+  return count(hand)
+  // console.log(fixed)
+  // console.log(Object.keys(fixed).length)
+  // console.log(duplicates(count(hand)))
+}
+
+function findTypeOfHand (hand) {
+  const handKeys = Object.keys(hand)
+  let str = ''
+  switch (handKeys.length) {
+    case 1:
+      str = 'Five of a kind!'
+      break
+    case 2:
+      str = 'Four of a kind'
+      console.log(hand)
+      if (hand[handKeys[0]] === 3 || hand[handKeys[1]] === 3) {
+        str = 'Full house'
+      }
+      break
+    case 3:
+      str = 'Three of a kind'
+      if (hand[handKeys[0]] === 2 || hand[handKeys[1]] === 2 || hand[handKeys[2]] === 2) {
+        str = 'Two pair'
+      }
+      break
+    case 4:
+      str = 'Pair'
+      break
+    case 5:
+      const sortedHand = sortNumbers(handKeys)
+      str = 'Runt'
+      if (sortedHand[0] === '1' && sortedHand[4] === '5') {
+        str = 'Low straight'
+      }
+      if (sortedHand[0] === '2' && sortedHand[4] === '6') {
+        str = 'High straight'
+      }
+      break
+  }
+
+  return str
 }
